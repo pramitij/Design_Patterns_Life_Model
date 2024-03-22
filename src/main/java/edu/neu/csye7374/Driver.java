72e4427531723613b5ca77fa48e0ae84893cded5
PramithiJagdish <pramitijagdish07@gmail.com>
2024-03-21 20:47:40 -0700

    Update Driver.java

--- src/main/java/edu/neu/csye7374/Driver.java
+++ src/main/java/edu/neu/csye7374/Driver.java
@@ -1,15 +1,12 @@
 package edu.neu.csye7374;
 
 /**
- * @author SaiAkhil
+ * @author PramithiJagdish
  */
 public class Driver {
     public static void main(String[] args) {
         System.out.println("============Main Execution Start===================\n\n");
-
-        //Dont add any new statements here
         Life.demo();
-
         System.out.println("\n\n============Main Execution End===================");
     }
 }
